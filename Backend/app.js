945ef47600af3fa33ea767a78eef3057a989982d
GoncaloCamaz <37366547+GoncaloCamaz@users.noreply.github.com>
2021-05-29 17:50:57 +0100

    Backend first steps

---
+++ Backend/app.js
@@ -0,0 +1,18 @@
+var express = require('express'),
+  app = express(),
+  port = process.env.PORT || 1234;
+var cors = require('cors')
+
+var mongoose = require('mongoose');
+
+app.use(cors());
+mongoose.connect('mongodb://mongo:27017/database', {useNewUrlParser: true, useUnifiedTopology: true })
+
+var routes = require("./api/Routes/routes")
+
+app.listen(port);
+
+console.log('API server started on: ' + port);
+app.use(express.urlencoded({ extended: true}))
+app.use(express.json())
+app.use('/api', routes);
